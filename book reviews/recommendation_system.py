e28e832daf43099f1e5c6f97b4203f86d673b978
Kaiwen Zheng <115329498+KaiwenZheng7609@users.noreply.github.com>
2023-03-28 23:17:43 -0400

    modified recommendation algorithm

--- book reviews/recommendation_system.py
+++ book reviews/recommendation_system.py
@@ -5,11 +5,14 @@ a recommendation system) to make recommendations.
 """
 from __future__ import annotations
 from typing import Any
-
+from book import Book
+# from python_ta.contracts import check_contracts
 
 SYSTEM_START = '*'
+MATCH_SCORE = 7
 
 
+# @check_contracts
 class RecommendationSystem:
     """A recommendation system based on the decision tree. This system is used when
     either the user has no account recorded or the user wants to explore books of genres
@@ -29,12 +32,13 @@ class RecommendationSystem:
         self.subsystems[subsystem.item] = subsystem
 
     def insert_attributes(self, book_attributes: list) -> None:
-        """TODO: ..."""
+        """Insert book_attributes into this recommendation system such that every next
+        entry in book_attributes is a child of the current one.
+        """
         self._insert_attributes_util(book_attributes, 0)
 
     def _insert_attributes_util(self, book_attributes: list, start: int) -> None:
-        """TODO: ...
-        """
+        """A helper method for RecommendationSystem.insert_attributes."""
         if start != len(book_attributes):
             if book_attributes[start] not in self.subsystems:
                 self.add_subsystem(RecommendationSystem(self.books, book_attributes[start]))
@@ -60,37 +64,42 @@ class RecommendationSystem:
         of questions provided by the user.
 
         Preconditions:
-            - len(responses) == 7
+            - len(responses) == 8
         """
-        return {int(book_id) for book_id in self._recommend_util(responses, 0)}
+        possible_recommended = self._recommend_util(responses, 0)
 
-    def _recommend_util(self, responses: list, start: int) -> list[str]:
-        """TODO: extract common parts"""
-        if start == len(responses):  # have reached one leaf node
-            all_recommended = list(self.subsystems.values())
-            return [subsystem.item for subsystem in all_recommended]
+        for i in range(len(possible_recommended) - 1, -1, -1):
+            book_attributes = self.books[possible_recommended[i]].get_attributes()
+            if _get_match_score(book_attributes, responses) < MATCH_SCORE:
+                possible_recommended.pop(i)
 
+        return set(possible_recommended)
+
+    def _recommend_util(self, responses: list, start: int) -> list[int]:
+        """A helper method for RecommendationSystem.recommend."""
+        if start == len(responses):
+            return list(self.subsystems[subsystem].item for subsystem in self.subsystems)
+
+        all_recommended = []
+        attribute = responses[start]
+        found = False
         if start == 0:
-            min_pages, max_pages = responses[start]
+            min_pages, max_pages = attribute
             for subsystem in self.subsystems:
-                num_pages = self.subsystems[subsystem].item
-                if min_pages <= num_pages <= max_pages:
-                    return self.subsystems[subsystem]._recommend_util(responses, start + 1)
-            else:
-                all_recommended = []
-                for subsystem in self.subsystems:
+                if min_pages <= self.subsystems[subsystem].item <= max_pages:
+                    found = True
                     all_recommended.extend(self.subsystems[subsystem]._recommend_util(responses, start + 1))
-                return all_recommended
         else:
-            attribute = responses[start]
             for subsystem in self.subsystems:
-                if subsystem == attribute:
-                    return self.subsystems[subsystem]._recommend_util(responses, start + 1)
-            else:
-                all_recommended = []
-                for subsystem in self.subsystems:
+                if attribute == self.subsystems[subsystem].item:
+                    found = True
                     all_recommended.extend(self.subsystems[subsystem]._recommend_util(responses, start + 1))
-                return all_recommended
+
+        if not found:
+            for subsystem in self.subsystems:
+                all_recommended.extend(self.subsystems[subsystem]._recommend_util(responses, start + 1))
+
+        return all_recommended
 
     # def height(self) -> int:
     #     if self.subsystems == {}:
@@ -122,6 +131,36 @@ class RecommendationSystem:
     #
     # def __str__(self) -> str:
     #     return self.str_rep(0)
+
+
+def _get_match_score(attributes: list, responses: list) -> int:
+    """Return the match score between attributes and responses.
+
+    Let attributes be the list of attributes of a given book defined in the method
+    Book.get_attributes and let responses be the user's reading preference. Let i be an integer
+    such that 0 <= i < min(len(attributes), len(responses)). Then we say responses[i] and
+    attributes[i] are matching entries if one of the following cases follows:
+
+        - i == 0 and responses[i][0] <= attributes[i] <= responses[i][1]
+        - i > 0 and responses[i] == attributes[i]
+
+    We define the match score between attributes and responses be the number of matching
+    entries between these two lists.
+
+    Preconditions:
+        - len(attributes) == 9
+        - len(responses) == 8
+        - isinstance(responses[0], tuple)
+    """
+    total = 0
+
+    for i in range(len(responses)):
+        if i == 0 and responses[i][0] <= attributes[i] <= responses[i][1]:
+            total += 1
+        elif i > 0 and attributes[i] == responses[i]:
+            total += 1
+
+    return total
 
 
 if __name__ == '__main__':
