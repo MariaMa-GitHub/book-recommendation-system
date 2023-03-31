42d214ba92def49af2e73573a82f4233cf35cc61
Kaiwen Zheng <115329498+KaiwenZheng7609@users.noreply.github.com>
2023-03-31 17:03:48 -0400

    book.py: test version 3

--- book reviews/book.py
+++ book reviews/book.py
@@ -1,71 +1,30 @@
 """
-Book Class
+Book Author
 """
 
-import pandas
+import gzip
+import json
 
 
-class Book:
+DATA_FILENAME = 'data/authors.json.gz'
+
+
+def load_author_data() -> dict[int, str]:
     """
-    Contain all the information of a book
-
-    Instance Attributes:
-    - book_id: book id
-    - title: book title
-    - is_ebook: whether this book has an e-book version
-    - authors: a mapping of author id to author name, represeting the author(s) of the book
-    - publisher: book publisher
-    - publication_year: publication year of the book
-    - country: book's country of origin
-    - language: language in which the book is written
-    - num_pages: number of pages of the book
-    - genres: a list of the genres of the book
-    - average_rating: the average of book's ratings
-    - ratings_count: the number of book's ratings
-    - description: description of the book
-    - similar_books: a set of similar books' ids
-    - link: Goodreads url of the book
+    Read and extract author json.gz data and return a mapping from author id to author name
     """
 
-    book_id: int
-    title: str
-    is_ebook: bool
-    authors: dict[int, str]
-    publisher: str
-    publication_year: int
-    country: str
-    language: str
-    num_pages: int
-    genres: set[str]
-    average_rating: float
-    ratings_count: int
-    description: str
-    similar_books: set[int]
-    link: str
+    data = []
+    with gzip.open(DATA_FILENAME) as fin:
+        for i in fin:
+            d = json.loads(i)
+            data.append(d)
 
-    def __init__(self, df: pandas.DataFrame, i: int):
-        """
-        Initialize book info using books dataframe
-        """
+    data = {int(author['author_id']): author['name'] for author in data}
 
-        self.book_id = int(df.iloc[i]['book_id'])
-        self.title = df.iloc[i]['title']
-        self.is_ebook = bool(df.iloc[i]['is_ebook'])
-        self.authors = {int(author['author_id']): '' for author in df.iloc[i]['authors']}
-        self.publisher = df.iloc[i]['publisher']
-        self.publication_year = int(df.iloc[i]['publication_year'])
-        self.country = df.iloc[i]['country_code']
-        self.language = df.iloc[i]['language_code']
-        self.num_pages = int(df.iloc[i]['num_pages'])
-        self.genres = {shelf['name'] for shelf in df.iloc[i]['popular_shelves'] if int(shelf['count']) >= 10}
-        self.average_rating = float(df.iloc[i]['average_rating'])
-        self.ratings_count = int(df.iloc[i]['ratings_count'])
-        self.description = df.iloc[i]['description']
-        self.similar_books = {int(book_id) for book_id in df.iloc[i]['similar_books']}
-        self.link = df.iloc[i]['link']
+    return data
 
-    def get_attributes(self) -> tuple:
-        """Return a list of attributes that are used in the tree-based recommendation system."""
-        return (self.num_pages, self.country, self.language,
-                self.title, frozenset(self.authors.keys()), self.publisher, self.publication_year,
-                self.is_ebook, self.book_id)
+
+if __name__ == '__main__':
+    # print(load_genre_data())
+    pass
